3820ffd4faa3906c3fcd23bf1d4c86904168e21b
AlissonCasalli <guiluir9876@gmail.com>
2026-05-05 17:08:24 -0300

    Feito os DTOs dos clientes, entidade e repositório.

---
+++ Exemplo Spring Aula/demo/src/main/java/com/example/demo/Entities/Cliente.java
@@ -0,0 +1,33 @@
+package com.example.demo.Entities;
+ 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+ 
+@Entity
+@Table(name = "clientes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cliente {
+ 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+ 
+    @Column(nullable = false)
+    private String nome;
+ 
+    @Column(nullable = false, unique = true)
+    private String email;
+ 
+    @Column(nullable = false)
+    private String telefone;
+ 
+    @Column(nullable = false)
+    private String endereco;
+ 
+    @Column(nullable = false, unique = true)
+    private String documento;
+}
